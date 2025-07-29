3b85e442649951a0782763b4eab5bbd98a758dae
mai11223213213213 <matvijko1606@gmail.com>
2025-07-29 18:18:52 +0300

    lklklkl

--- src/components/Battle.tsx
+++ src/components/Battle.tsx
@@ -1,14 +1,14 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import AbilitiesMenu from './AbilitiesMenu';
 
 export interface Character {
   CharacterName: string;
   maxHealth: number;
-  currHealth: number;        
-  setCurH: (value: number) => void;  
+  // currHealth: number;        
+  // setCurH: (value: number) => void;  
   maxMana: number;
-  currMana: number;         
-  setCurM: (value: number) => void;  
+  // currMana: number;         
+  // setCurM: (value: number) => void;  
   abilities: ability[];
 }
 export type ability = {
@@ -29,14 +29,14 @@ class TestChar implements Character{
     
     maxMana: number = 100;
    
-    private healthState = useState(this.maxHealth);
-    currHealth = this.healthState[0];  
-    setCurH = this.healthState[1];     
+    // private healthState = useState(this.maxHealth);
+    // currHealth = this.healthState[0];  
+    // setCurH = this.healthState[1];     
 
   
-    private manaState = useState(0);
-    currMana = this.manaState[0];      // Current value
-    setCurM = this.manaState[1];       // Setter
+    // private manaState = useState(0);
+    // currMana = this.manaState[0];      // Current value
+    // setCurM = this.manaState[1];       // Setter
     
 
     abilities : ability[] = [
@@ -88,11 +88,39 @@ const FirstMove = () => {
       return "player2"
   }
 }
+
+
 let Player1 = new TestChar()
 let Player2 = new TestChar()
 let WhoMove = FirstMove()
 let Player1Move = null
 let Player2Move = null 
+
+let [Player1CurrHp, setPl1CHp] = useState(Player1.maxHealth)
+let [Player1CurrMana, setPl1CMana] = useState(0)
+let [Pl1HpPercent, setPl1HpP] =useState(0)
+let [Pl1ManaPercent, setPl1ManaP] =useState(0)
+
+let [Player2CurrHp, setPl2CHp] = useState(Player2.maxHealth)
+let [Player2CurrMana, setPl2CMana] = useState(0)
+let [Pl2HpPercent, setPl2HpP] =useState(0)
+let [Pl2ManaPercent, setPl2ManaP] =useState(0)
+
+useEffect(()=>{
+  let P1CHPer = Player1CurrHp/Player1.maxHealth * 100
+  setPl1HpP(P1CHPer)
+
+  let P1CMPer = Player1CurrMana/Player1.maxMana * 100
+  setPl1ManaP(P1CMPer)
+
+  let P2CHPer = Player2CurrHp/Player2.maxHealth * 100
+  setPl2HpP(P2CHPer)
+
+  let P2CMPer = Player2CurrMana/Player2.maxMana * 100
+  setPl2ManaP(P2CMPer)
+}, [Player1CurrHp, Player1CurrMana,Player2CurrHp,Player2CurrMana])
+
+
 let [abititiesMenu, setAbM] = useState<Character>()
 let [currType , serCurrType] = useState<string>()
 const Abilities = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -123,8 +151,29 @@ const Abilities = (event: React.MouseEvent<HTMLButtonElement>) => {
       <div className="AbillitiesArea">
         {abititiesMenu && <AbilitiesMenu player={abititiesMenu} type={currType}/>}
       </div>
+      <div className="players-info">
+        <div className="player1">
+          <div className="player1-name">{Player1.CharacterName}</div>
+          <div className="player1-hpBar">
+            <div className="player1-hpStatus" style={{width:`${Pl1HpPercent}%`}}>HP: {Player1CurrHp}/{Player1.maxHealth}</div>
+          </div>
+          <div className="player1-manaBar">
+            <div className="player1-manaStatus" style={{width:`${Pl1ManaPercent}%`}}>MP: {Player1CurrMana}/{Player1.maxMana}</div>
+          </div>
+
+        </div>
+        <div className="player2">
+        <div className="player2-name">{Player2.CharacterName}</div>
+          <div className="player2-hpBar">
+            <div className="player2-hpStatus" style={{width:`${Pl2HpPercent}%`}}>HP: {Player2CurrHp}/{Player2.maxHealth}</div>
+          </div>
+          <div className="player2-manaBar">
+            <div className="player2-manaStatus" style={{width:`${Pl2ManaPercent}%`}}>MP: {Player2CurrMana}/{Player2.maxMana}</div>
+          </div>
+
+        </div>
+      </div>
       <div className="navigation">
-        <p className="player1-name"> {Player1.CharacterName}</p>
         <button className="attack"  onClick={Abilities}>Attack</button>
         <button className="protection" onClick={Abilities}>Defence</button>
         <button className="heal" onClick={Abilities}>Heal</button>
